55a6fd97203a468b66f8e601029ccb681a79c607
Dragonkeun <dragonkeun7@gmail.com>
2023-04-14 12:42:18 +0900

    TodoList 할일 추가 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import TodoBoard from "./components/TodoBoard";
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const addItem = () => {
+    console.log(inputValue);
+    setTodoList([...todoList, inputValue]); // 기존에 todoList 배열에 있는 값은 유지하며 추가로 inputValue 값을 추가함
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(event) => {
+          setInputValue(event.target.value);
+        }}
+      />
+      <button onClick={addItem}>추가</button>
+      <TodoBoard todoList={todoList} />
+    </main>
   );
 }
 
